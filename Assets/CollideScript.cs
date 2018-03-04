1e5884e2590982d07635bc6b203944a67175327d
Charles23R <renaudcharles14@hotmail.com>
2018-03-04 02:10:36 -0500

    Scene facile finie (sauf chrono et perdre) Scene difficile pas bonne Jai le background de l'espace

---
+++ Assets/CollideScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollideScript : MonoBehaviour {
+
+       bool loose = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.CompareTag("spr_horseSpriteSheet_5") || other.gameObject.CompareTag("spr_horseSpriteSheet_9") || other.gameObject.CompareTag("spr_horseSpriteSheet_13"))
+        {
+            loose = true;
+        }
+    }
+
+    public bool getLoose()
+    {
+        return loose;
+    }
+}
